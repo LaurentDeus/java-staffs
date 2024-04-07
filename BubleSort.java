9782d08671c8df9dbd076f92f052998c32a7c675
LaurentDeus <laurentdeusdedith75@gmail.com>
2024-04-07 14:09:16 +0300

    Implemented Bubble sort

---
+++ BubleSort.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class GuessGame {
+
+private static int[] sortArray(int[] arr){
+
+	int arr_len = arr.length;
+
+	for(int j=0;j < arr_len - 1;j++){
+
+		for(int i = 1;i<arr_len - j;i++){
+
+		if(arr[i] > arr[i-1]){	
+			int temp = arr[i];
+			arr[i] = arr[i-1];
+			arr[i-1] = temp;	
+			}
+		
+		}
+	}
+		return arr;
+
+}
+    public static void main(String[] args) {
+
+    int[] age = {23,12,25,2,155};
+	System.out.println("Before Sorted: "+ Arrays.toString(age));	
+
+	System.out.println("After Sorted: "+ Arrays.toString(sortArray(age)));
+	System.out.println("Original Array: "+ Arrays.toString(age));
+    }
+}
